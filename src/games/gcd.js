9289a51cb6e29f7d57ab38f7fa95d07c5c8ba4f9
psina <unnamedbanana@gmail.com>
2022-05-29 19:51:05 +0300

    fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,22 +7,27 @@ const findGCD = () => {
   const condition = 'Find the greatest common divisor of given numbers.';
 
   const generateQuestionAndRigthAnswer = () => {
-    const question = `${Math.floor(Math.random() * 100)} ${Math.floor(Math.random() * 100)}`;
+    const pairOfRandomNumbers = cons(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
+    const question = `${car(pairOfRandomNumbers)} ${cdr(pairOfRandomNumbers)}`;
 
-    const getPair = (first, second) => (first < second ? cons(second, first) : cons(first, second));
+    const getPair = (first, second) => (first <= second ? cons(second, first) : cons(first, second));
 
-    const [firstNum, secondNum] = question.split(' ');
-    const arrayOfNumbers = [Number(firstNum), Number(secondNum)];
-    const [first, second] = arrayOfNumbers;
-
-    let pair = getPair(first, second);
+    let pair = getPair(car(pairOfRandomNumbers), cdr(pairOfRandomNumbers));
     let reminder = car(pair) % cdr(pair);
     let result = cdr(pair);
 
-    while (reminder !== 0) {
-      pair = getPair(reminder, result);
-      reminder = car(pair) % cdr(pair);
-      result = cdr(pair);
+    if (car(pair) === 0 && cdr(pair) === 0) {
+      result = 0;
+    } else if (car(pair) === 0) {
+      result = Math.abs(cdr(pair));
+    } else if (cdr(pair) === 0) {
+      result = Math.abs(car(pair));
+    } else {
+      while (reminder !== 0) {
+        pair = getPair(reminder, result);
+        reminder = car(pair) % cdr(pair);
+        result = Math.abs(cdr(pair));
+      }
     }
 
     return cons(question, String(result));
